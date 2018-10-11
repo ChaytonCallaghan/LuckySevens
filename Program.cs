3866df5ee9917039bd0dc2805ffefeb81a03b1e0
ChaytonCallaghan <chaytoncallaghan@gmail.com>
2018-10-11 13:35:49 -0700

    3PM Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LuckySevens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalnumberofSevens = 0;
+            Random rng = new Random();
+
+            int die1;
+            int die2;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                die1 = rng.Next(1, 7);
+                die2 = rng.Next(1, 7);
+
+                if (die1 + die2 == 7)
+                totalnumberofSevens++;
+            }
+
+            Console.WriteLine("Out of 100 rolls, you rolled {0} sevens", totalnumberofSevens);
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # LuckySevens
+
+
+
+MDREADME
+
+
+Student ID: ChaytonCallaghan or ChaytonC1
+
+Date Started: Oct,03,2018   
+Date Completed: Oct,03,2018
